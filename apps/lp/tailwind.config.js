8348fa149006d201ccf055b3d5d7e84afd95f160
junaid <junaid.saleem.10144@gmail.com>
2023-04-26 13:09:32 +0500

    add custom font

--- apps/lp/tailwind.config.js
+++ apps/lp/tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+  content: ['./src/**/*.{html,js,ts,tsx,jsx}'],
   theme: {
     extend: {
-      backgroundImage:{
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-radial-hero":"radial-gradient(farthest-corner at 20% 50%,#ffffffff 0%,#88A4BF 120%);"
-      }
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-radial-hero':
+          'radial-gradient(farthest-corner at 20% 50%,#ffffffff 0%,#88A4BF 120%);',
+      },
+      fontFamily: {
+        title: ["'Montserrat'"],
+      },
     },
   },
   plugins: [],
 }
-
