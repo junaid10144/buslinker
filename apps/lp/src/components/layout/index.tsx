708b55d9030644af981201ec4d542e780f46158b
junaid <junaid.saleem.10144@gmail.com>
2023-05-02 19:10:16 +0500

    add footer

--- apps/lp/src/components/layout/index.tsx
+++ apps/lp/src/components/layout/index.tsx
@@ -1,16 +1,15 @@
-import React from "react"
-import tw from "tailwind-styled-components"
+import React from 'react'
+import tw from 'tailwind-styled-components'
+import { Footer } from './footer'
 
 const Container = tw.div``
-export * from "./seo"
+export * from './seo'
 
 export const Layout = ({ children }) => {
-
   return (
     <Container>
-        <main>{children}</main>
-      </Container>
+      <main>{children}</main>
+      <Footer />
+    </Container>
   )
 }
-
-
