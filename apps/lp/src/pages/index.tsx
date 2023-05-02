c584c51de5691a3f901661de55c1a43454b0ad60
junaid <junaid.saleem.10144@gmail.com>
2023-05-02 21:31:11 +0500

    add basic stats section

--- apps/lp/src/pages/index.tsx
+++ apps/lp/src/pages/index.tsx
@@ -6,6 +6,7 @@ import { GiAlarmClock } from '@react-icons/all-files/gi/GiAlarmClock'
 import { GiMoneyStack } from '@react-icons/all-files/gi/GiMoneyStack'
 import { BiNetworkChart } from '@react-icons/all-files/bi/BiNetworkChart'
 import { FaLeaf } from '@react-icons/all-files/fa/FaLeaf'
+import { StatsSection } from 'sections'
 
 const sections = {
   features: {
@@ -98,6 +99,7 @@ const IndexPage = () => {
       <HeroSection />
       <MidSection section={sections.features} />
       <MidSection section={sections.features2} />
+      <StatsSection />
     </Layout>
   )
 }
