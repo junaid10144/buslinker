cc4f4da90e8234a3416cc00bb7b3c355638e00c2
junaid <junaid.saleem.10144@gmail.com>
2023-05-04 19:15:40 +0500

    extract some of the BF components

---
+++ apps/lp/src/components/input-core/index.tsx
@@ -0,0 +1,4 @@
+import tw from 'tailwind-styled-components'
+
+//export const InputCore = tw.input`relative w-full peer flex h-10 focus:outline-none appearance-none border border-gray-300 rounded-full outline-none transition pl-4 pr-6 group-hover:border-green-500 focus:border-green-500 cursor-pointer`
+export const InputCore = tw.input`appearance-none border rounded-xl w-full outline-none pl-4 pr-6 focus:outline-none focus:ring-0 h-10`
