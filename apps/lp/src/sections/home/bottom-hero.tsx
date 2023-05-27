ee218ff1a582ea8393f5be5ade5b8a0553d36389
Junaid <junaid.saleem.10144@gmail.com>
2023-05-27 09:25:52 +0500

    animate bottom hero section

--- apps/lp/src/sections/home/bottom-hero.tsx
+++ apps/lp/src/sections/home/bottom-hero.tsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import { Button, SectionAndOffset, Text } from 'components'
 import { LogoTitle } from './hero'
+import { InView } from 'react-intersection-observer'
 export const BottomHeroSection = () => (
-  <SectionAndOffset>
-    <div className="flex flex-col items-center">
-      <LogoTitle />
-    </div>
-    <div className="flex flex-col items-center w-1/3 text-center mx-auto mt-8">
-      <Text>
-        {
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt eveniet soluta error distinctio quibusdam rerum ab unde nihil, corporis aspernatur veniam fugiat, corrupti voluptate totam illo, provident ipsa? Nulla, dolorem.'
-        }
-      </Text>
-      <div className="mt-8">
-        <Button>Book Now</Button>
-      </div>
-    </div>
-  </SectionAndOffset>
+  <InView>
+    {({ ref, inView }) => (
+      <SectionAndOffset ref={ref}>
+        <div className="flex flex-col items-center">
+          <LogoTitle inView={inView} />
+        </div>
+        <div
+          className={`flex flex-col items-center w-1/3 text-center mx-auto mt-8 transition delay-[1400ms] duration-700 ${
+            inView ? '' : 'opacity-0'
+          }`}
+        >
+          <Text>
+            {
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt eveniet soluta error distinctio quibusdam rerum ab unde nihil, corporis aspernatur veniam fugiat, corrupti voluptate totam illo, provident ipsa? Nulla, dolorem.'
+            }
+          </Text>
+          <div className="mt-8">
+            <Button>Book Now</Button>
+          </div>
+        </div>
+      </SectionAndOffset>
+    )}
+  </InView>
 )
