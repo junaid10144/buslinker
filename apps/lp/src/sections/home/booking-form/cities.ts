6ba79b1f0e8b312616cfa0b4df52ceb53908785e
Junaid <junaid.saleem.10144@gmail.com>
2023-06-04 16:50:45 +0500

    show real cities on search page taken from api

--- apps/lp/src/sections/home/booking-form/cities.ts
+++ apps/lp/src/sections/home/booking-form/cities.ts
@@ -1,1 +1,6 @@
-export const cities = ['Karachi', 'Lahore', 'Islamabad']
+export const cities = [
+  { label: 'Karachi', value: 'karachi' },
+  { label: 'Islamabad', value: 'islamabad' },
+  { label: 'Lahore', value: 'lahore' },
+  { label: 'Mandi Bahauddin', value: 'mandi-bahauddin' },
+]
